780a2d6db7e898c972329489c1614041f7d6eec1
ANDRE SALVADEO <andre.salvadeo@viavarejo.com.br>
2022-05-31 17:36:51 -0300

    Testes do Serviço GetAll e GetById

---
+++ tests/Domain/Dto/Fakers/RegraResumoDtoFaker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Bogus;
+
+using Domain.Dto;
+using Domain.Fakers;
+
+namespace DomainTests.Dto.Fakers;
+
+internal sealed class RegraResumoDtoFaker : Faker<RegraResumoDto>
+{
+    public RegraResumoDtoFaker() : base(FakerConstants.Locale)
+    {
+        RuleFor(e => e.Id, fk => fk.Random.Hash());
+        RuleFor(e => e.Nome, fk => fk.Lorem.Sentence());
+        RuleFor(e => e.DataInclusao, fk => fk.Date.Recent(2));
+        RuleFor(e => e.IncluidoPor, fk => fk.Person.FullName);
+    }
+
+    public static IEnumerable<RegraResumoDto> GerarLista()
+        => (new RegraResumoDtoFaker()).Generate((new Faker().Random.Int(1, 10)));
+}
---
+++ tests/Domain/Services/RegrasServiceTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+using Bogus;
+
+using Domain.Dto;
+using Domain.Dto.Fakers;
+using Domain.Entities;
+using Domain.Entities.Fakers;
+using Domain.Repositories;
+
+using DomainTests.Dto.Fakers;
+
+using FluentAssertions;
+
+using Moq;
+using Moq.AutoMock;
+
+using Xunit;
+
+namespace Domain.Services;
+
+
+public class RegrasServiceTests
+{
+    private readonly AutoMocker _autoMoq;
+    private readonly Faker _faker;
+
+    public RegrasServiceTests()
+    {
+        _autoMoq = new AutoMocker();
+        _faker = new Faker();
+    }
+
+    [Fact]
+    public void AdicionarRegraPeloRepositorio()
+    {
+        // Setup
+        RegraDto dto = new RegraDtoFaker();
+        Mock<RegraRepository> mock = _autoMoq.GetMock<RegraRepository>();
+        mock.Setup(repo => repo.Add(It.IsAny<Regra>()))
+            .Callback<Regra>((regra) => regra.Id = _faker.Random.Hash())
+            .Verifiable();
+        
+        // Execute
+        var service = _autoMoq.CreateInstance<RegrasService>();
+        Regra resultado = service.Adicionar(dto);
+
+        // Validar
+        mock.Verify();
+        resultado.Should().NotBeNull()
+            .And
+            .BeEquivalentTo(dto, cfg => cfg.ExcludingMissingMembers());
+        resultado.Id.Should().NotBeNullOrEmpty();
+
+    }
+
+    [Fact]
+    public void ObterRegraPorId()
+    {
+        // Setup
+        Regra regra = new RegraFaker();
+        string id = _faker.Random.Hash();
+
+        Mock<RegraRepository> mock = _autoMoq.GetMock<RegraRepository>();
+        mock.Setup(repo => repo.GetRegraById(id))
+            .Returns(regra)
+            .Verifiable();
+
+        // Execute
+        var service = _autoMoq.CreateInstance<RegrasService>();
+        Regra resultado = service.ObterPorId(id);
+
+        // Validar
+        mock.Verify();
+        resultado.Should().BeSameAs(regra);
+    }
+
+    [Fact]
+    public void ListarTodas()
+    {
+        // Setup
+        IEnumerable<RegraResumoDto> lista = RegraResumoDtoFaker.GerarLista();
+
+        Mock<RegraRepository> mock = _autoMoq.GetMock<RegraRepository>();
+        mock.Setup(repo => repo.GetAll())
+            .Returns(lista)
+            .Verifiable();
+
+        // Execute
+        var service = _autoMoq.CreateInstance<RegrasService>();
+        IEnumerable<RegraResumoDto> resultado = service.ListarTodas();
+
+        // Validar
+        mock.Verify();
+        resultado.Should().BeSameAs(lista);
+        resultado.Should().BeEquivalentTo(lista);
+
+    }
+}
+
