0bb456720b8ceabb443a6525d4cb8a82f5fe739a
Andre Salvadeo <andresalvadeo@hotmail.com>
2022-08-13 14:37:03 -0300

    Configuração do Serilog

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,35 +1,79 @@
 using Api.Extensions;
-
 using Domain.Extensions;
-
 using Repositories.Extensions;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(GetAppConfiguration(args))
+    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddMappingProfiles();
-builder.Services.AddDomainServicesAndValidators();
-builder.Services.AddMongoDBRepositories(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Iniciando a aplicacao");
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
+    Log.Information("Serilog configurado");
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    Log.Information("Configurados servicos padrao");
+
+    builder.Services.AddMappingProfiles();
+    builder.Services.AddDomainServicesAndValidators();
+    builder.Services.AddMongoDBRepositories(builder.Configuration);
+    Log.Information("Configurados servicos da aplicacao");
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    Log.Information("Aplicacao configurada");
+
+    Log.Information("Inciando aplicacao");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
+static IConfiguration GetAppConfiguration(string[] args)
+{
+    string enviroment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production";
+    IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 
-app.UseAuthorization();
+    PreparerConfigBuilder(configurationBuilder, enviroment, args);
 
-app.MapControllers();
+    return configurationBuilder.Build();
+}
 
-app.Run();
+static void PreparerConfigBuilder(IConfigurationBuilder configurationBuilder, string enviroment, string[] args)
+{
+    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+        .AddJsonFile($"appsettings.{enviroment}.json", optional: true, reloadOnChange: false);
+
+    if (enviroment == "Development")
+        configurationBuilder.AddUserSecrets<Program>(optional: true);
+
+    configurationBuilder.AddEnvironmentVariables()
+        .AddCommandLine(args);
+}
